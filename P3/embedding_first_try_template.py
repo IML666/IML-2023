cacc8648fea6440baa0746fefb1a8ed0c851b7b1
fmrcarica <fmrcarica@student.ethz.ch>
2023-05-01 13:03:45 +0200

    Added new code for embeddings

---
+++ P3/embedding_first_try_template.py
@@ -0,0 +1,253 @@
+# This serves as a template which will guide you through the implementation of this task.  It is advised
+# to first read the whole template and get a sense of the overall structure of the code before trying to fill in any of the TODO gaps
+# First, we import necessary libraries:
+import numpy as np
+from torchvision import transforms
+from torch.utils.data import DataLoader, TensorDataset
+import os
+import torch
+from torchvision import transforms
+import torchvision.datasets as datasets
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.models import resnet50, ResNet50_Weights
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+from torchvision import models
+from torch.nn import CrossEntropyLoss
+import os
+import numpy as np
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def generate_embeddings():
+    """
+    Transform, resize and normalize the images and then use a pretrained model to extract 
+    the embeddings.
+    """
+    train_transforms = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    train_dataset = datasets.ImageFolder(root="dataset/", transform=train_transforms)
+    # Hint: adjust batch_size and num_workers to your PC configuration, so that you don't 
+    # run out of memory
+    train_loader = DataLoader(dataset=train_dataset,
+                              batch_size=64,
+                              shuffle=False,
+                              pin_memory=True, num_workers=16)
+
+    # TODO: define a model for extraction of the embeddings (Hint: load a pretrained model,
+    #  more info here: https://pytorch.org/vision/stable/models.html)
+    model = models.resnet50(pretrained=True)
+    embeddings = []
+    embedding_size = 1000 # Dummy variable, replace with the actual embedding size once you 
+    # pick your model
+    num_images = len(train_dataset)
+    embeddings = np.zeros((num_images, embedding_size))
+    # TODO: Use the model to extract the embeddings. Hint: remove the last layers of the 
+    # model to access the embeddings the model generates. 
+    
+    # Select the desired layer
+    layer = model._modules.get('avgpool')
+    
+    def copy_embeddings(m, i, o):
+        """
+        Copy embeddings from the penultimate layer.
+        """
+        o = o[:, :, 0, 0].detach().numpy().tolist()
+        outputs.append(o)
+    
+    outputs = []
+    # attach hook to the penulimate layer
+    _ = layer.register_forward_hook(copy_embeddings)
+       
+    model.eval() # Inference mode
+
+    for image, label in train_loader:
+        _ = model(image)
+        
+    print(len(outputs)) #returns 100000
+    
+    #embeddings = [item for sublist in outputs for item in sublist]
+    embeddings = outputs
+
+    print(len(embeddings)) # returns 10000
+    print(np.array(embeddings[0]).shape) #returns (64,2018)
+
+    np.save('dataset/embeddings.npy', embeddings)
+    
+    
+
+
+
+def get_data(file, train=True):
+    """
+    Load the triplets from the file and generate the features and labels.
+
+    input: file: string, the path to the file containing the triplets
+          train: boolean, whether the data is for training or testing
+
+    output: X: numpy array, the features
+            y: numpy array, the labels
+    """
+    triplets = []
+    with open(file) as f:
+        for line in f:
+            triplets.append(line)
+
+    # generate training data from triplets
+    train_dataset = datasets.ImageFolder(root="dataset/",
+                                         transform=None)
+    filenames = [s[0].split('/')[-1].replace('.jpg', '') for s in train_dataset.samples]
+    embeddings = np.load('dataset/embeddings.npy')
+    # TODO: Normalize the embeddings across the dataset
+
+    file_to_embedding = {}
+    for i in range(len(filenames)):
+        file_to_embedding[filenames[i]] = embeddings[i]
+    X = []
+    y = []
+    # use the individual embeddings to generate the features and labels for triplets
+    for t in triplets:
+        emb = [file_to_embedding[a] for a in t.split()]
+        X.append(np.hstack([emb[0], emb[1], emb[2]]))
+        y.append(1)
+        # Generating negative samples (data augmentation)
+        if train:
+            X.append(np.hstack([emb[0], emb[2], emb[1]]))
+            y.append(0)
+    X = np.vstack(X)
+    y = np.hstack(y)
+    return X, y
+
+# Hint: adjust batch_size and num_workers to your PC configuration, so that you don't run out of memory
+def create_loader_from_np(X, y = None, train = True, batch_size=64, shuffle=True, num_workers = 4):
+    """
+    Create a torch.utils.data.DataLoader object from numpy arrays containing the data.
+
+    input: X: numpy array, the features
+           y: numpy array, the labels
+    
+    output: loader: torch.data.util.DataLoader, the object containing the data
+    """
+    if train:
+        dataset = TensorDataset(torch.from_numpy(X).type(torch.float), 
+                                torch.from_numpy(y).type(torch.long))
+    else:
+        dataset = TensorDataset(torch.from_numpy(X).type(torch.float))
+    loader = DataLoader(dataset=dataset,
+                        batch_size=batch_size,
+                        shuffle=shuffle,
+                        pin_memory=True, num_workers=num_workers)
+    return loader
+
+# TODO: define a model. Here, the basic structure is defined, but you need to fill in the details
+class Net(nn.Module):
+    """
+    The model class, which defines our classifier.
+    """
+    def __init__(self):
+        """
+        The constructor of the model.
+        """
+        super().__init__()
+        self.fc1 = nn.Linear(4,6)
+        self.fc2 = nn.Linear(6,6)                                   
+        self.out = nn.Linear(6, 3)      
+
+    def forward(self, x):
+        """
+        The forward pass of the model.
+
+        input: x: torch.Tensor, the input to the model
+
+        output: x: torch.Tensor, the output of the model
+        """
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.out(x)
+        return x
+
+def train_model(train_loader):
+    """
+    The training procedure of the model; it accepts the training data, defines the model 
+    and then trains it.
+
+    input: train_loader: torch.data.util.DataLoader, the object containing the training data
+    
+    output: model: torch.nn.Module, the trained model
+    """
+    model = Net()
+    model.train()
+    model.to(device)
+    n_epochs = 10
+    # TODO: define a loss function, optimizer and proceed with training. Hint: use the part 
+    # of the training data as a validation split. After each epoch, compute the loss on the 
+    # validation split and print it out. This enables you to see how your model is performing 
+    # on the validation data before submitting the results on the server. After choosing the 
+    # best model, train it on the whole training data.
+    
+    criterion = CrossEntropyLoss()
+    optimizer = torch.optim.Adam(mymodel.parameters(), lr=0.01)
+
+    for epoch in range(n_epochs):        
+        for [X, y] in train_loader:
+            pass
+    return model
+
+def test_model(model, loader):
+    """
+    The testing procedure of the model; it accepts the testing data and the trained model and 
+    then tests the model on it.
+
+    input: model: torch.nn.Module, the trained model
+           loader: torch.data.util.DataLoader, the object containing the testing data
+        
+    output: None, the function saves the predictions to a results.txt file
+    """
+    model.eval()
+    predictions = []
+    # Iterate over the test data
+    with torch.no_grad(): # We don't need to compute gradients for testing
+        for [x_batch] in loader:
+            x_batch= x_batch.to(device)
+            predicted = model(x_batch)
+            predicted = predicted.cpu().numpy()
+            # Rounding the predictions to 0 or 1
+            predicted[predicted >= 0.5] = 1
+            predicted[predicted < 0.5] = 0
+            predictions.append(predicted)
+        predictions = np.vstack(predictions)
+    np.savetxt("results.txt", predictions, fmt='%i')
+
+
+# Main function. You don't have to change this
+if __name__ == '__main__':
+    TRAIN_TRIPLETS = 'train_triplets.txt'
+    TEST_TRIPLETS = 'test_triplets.txt'
+
+    # generate embedding for each image in the dataset
+    if(os.path.exists('dataset/embeddings.npy') == False):
+        generate_embeddings()
+'''
+
+    # load the training and testing data
+    X, y = get_data(TRAIN_TRIPLETS)
+    X_test, _ = get_data(TEST_TRIPLETS, train=False)
+
+    # Create data loaders for the training and testing data
+    train_loader = create_loader_from_np(X, y, train = True, batch_size=64)
+    test_loader = create_loader_from_np(X_test, train = False, batch_size=2048, shuffle=False)
+
+    # define a model and train it
+    model = train_model(train_loader)
+    
+    # test the model on the test data
+    test_model(model, test_loader)
+    print("Results saved to results.txt")
+'''
